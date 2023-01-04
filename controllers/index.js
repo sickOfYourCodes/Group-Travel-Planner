6e48d0f3e742e8ab42639164fe524a2c45a1a97f
Brian Wang <wangbrian26@gmail.com>
2023-01-04 13:28:44 -0800

    updated routes

--- controllers/index.js
+++ controllers/index.js
@@ -6,6 +6,8 @@ const homeRoute = require("./homeRoute.js");
 const dashBoardRoute = require("./dashBoardRoute.js");
 const loginRoute = require("./loginRoute.js");
 const signupRoute = require("./signupRoute.js");
+const faqRoute = require("./faq.js");
+const aboutRoute = require("./about.js");
 
 // Allows the server to know where to send the routes
 
@@ -14,6 +16,8 @@ router.use("/", homeRoute);
 router.use("/dashboard", dashBoardRoute);
 router.use("/login", loginRoute);
 router.use("/signup", signupRoute);
+router.use("/faq", faqRoute);
+router.use("/about", aboutRoute);
 
 router.get("*", (req, res) => {
   window.location.href = "/";
