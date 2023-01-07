6e4c3e321a7445b4c0a89b2e459272641794bf8c
Brian Wang <wangbrian26@gmail.com>
2023-01-06 20:08:47 -0800

    updated logout once you are logged in

--- controllers/about.js
+++ controllers/about.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
+  if (req.session.loggedIn) {
+    res.status(200).render("about", { layout: "user" });
+  }
   res.status(200).render("about");
 });
 
